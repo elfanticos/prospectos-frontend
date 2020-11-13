1b086bcff5864068307da918f47ddc8eb3a9da7c
jmeza <jhonatan.meza01@gmail.com>
2020-11-13 20:50:09 +0100

    commit

--- prospectos-frontend/src/app/app-routing.module.ts
+++ prospectos-frontend/src/app/app-routing.module.ts
@@ -3,10 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './core/auth/login/login.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule),
-  },
+  // {
+  //   path: '',
+  //   loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule),
+  // },
   {
     path: 'login',
     component: LoginComponent
@@ -16,7 +16,7 @@ const routes: Routes = [
     loadChildren: () => import('./modules/prospecto/prospecto.module').then(m => m.ProspectoModule),
   },
   {
-    path: 'registro',
+    path: '',
     loadChildren: () => import('./modules/registro/registro.module').then(m => m.RegistroModule),
   },
 
