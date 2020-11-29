6338035dc9940db033ca33710c3eb644d3631a6e
jmeza <jhonatan.meza01@gmail.com>
2020-11-29 19:40:14 +0100

    commit

--- prospectos-frontend/src/app/modules/registro/components/informacion-pc/informacion-pc.component.ts
+++ prospectos-frontend/src/app/modules/registro/components/informacion-pc/informacion-pc.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { IDisco } from '../../entities/combos/disco';
 import { IProcesador } from '../../entities/combos/procesador';
@@ -18,6 +18,8 @@ export class InformacionPcComponent implements OnInit {
   memoriasRam: IRam[] = [];
   discos: IDisco[] = [];
   sistemas: ISistema[] = [];
+  @ViewChild('fileTxt') fileTxt: ElementRef;
+  filesToUpload: Array<File>;
   constructor(
     private _registroFormService: RegistroFormService,
     private _combosService: CombosService
@@ -42,8 +44,8 @@ export class InformacionPcComponent implements OnInit {
   get memoriaRam() { return this.infoPcForm.get('equipo').get('memoriaRam'); }
   get discoDuro() { return this.infoPcForm.get('equipo').get('discoDuro'); }
   get sistOperativo() { return this.infoPcForm.get('equipo').get('sistOperativo'); }
-  get urlImagen() { return  this.infoPcForm.get('image').get('urlImagen'); }
-  get nombreImagen() { return  this.infoPcForm.get('image').get('nombreImagen'); }
+  get urlImagen() { return this.infoPcForm.get('image').get('urlImagen'); }
+  get nombreImagen() { return this.infoPcForm.get('image').get('nombreImagen'); }
 
   loadProcesadores(): void {
     this._combosService.comboProcesador().subscribe(procesadores => {
@@ -73,4 +75,61 @@ export class InformacionPcComponent implements OnInit {
     });
   }
 
+
+  /**
+   * @author jmeza
+   */
+  adjuntarImagen(): void {
+    // Seleccionar el objeto
+
+    // Crear el evento de click
+    let event = new MouseEvent('click', { bubbles: true });
+
+    // Setear el input file
+    this.fileTxt.nativeElement.value = null;
+
+    // Disparar el evento click al input file
+    this.fileTxt.nativeElement.dispatchEvent(event);
+
+  }
+
+  fileChangeEvent(fileInput: any): void {
+    //Obtener el objeto del input file
+    this.filesToUpload = <Array<File>>fileInput.target.files;
+
+    /*************** VALIDACIONES ***************/
+    if (this.filesToUpload.length == 0) {
+      this.fileTxt.nativeElement.value = "";
+      console.log('Seleccione un PDF');
+      return;
+    }
+
+    // Obtener el nombre del archivo subido
+    let nameFile: string = this.filesToUpload[0].name;
+    if (nameFile.split('.')[1] == undefined) {
+      this.fileTxt.nativeElement.value = "";
+      console.log('Archivo incorrecto sin extension');
+      return;
+    }
+
+    // if (['pdf'].indexOf(this.filesToUpload[0].type.split('/')[1].toLowerCase()) < 0) {
+    //   this.fileTxt.nativeElement.value = "";
+    //   console.log('Seleccione un archivo de tipo .PDF');
+    //   return;
+    // }
+
+    if (this.filesToUpload[0].size / 1024 / 1024 >= 2) {
+      this.fileTxt.nativeElement.value = "";
+      console.log('Seleccione un archivo menor a 2MB');
+    }
+    /*******************************************************/
+
+    // Renderizar el archivo subido para obtener el base64
+    let render = new FileReader();
+    render.readAsDataURL(this.filesToUpload[0]);
+    render.onload = (event) => {
+      console.log('onload render');
+      console.log(event);
+    };
+  }
 }
