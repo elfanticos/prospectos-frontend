2b10f466c70d76ea6bd10a4e0b55d98fc237ae6e
joseminaya <jose.minayac15@gmail.com>
2020-12-03 15:35:34 -0500

    footer

--- prospectos-frontend/src/app/app.module.ts
+++ prospectos-frontend/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AgmCoreModule } from '@agm/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -20,6 +21,7 @@ import { AgmCoreModule } from '@agm/core';
       // https://developers.google.com/maps/documentation/javascript/get-api-key?hl=en
       apiKey: 'AIzaSyBOhg8YYoI8oFGAGTrP1O_4r9b4i_KUNH0'
   }),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
